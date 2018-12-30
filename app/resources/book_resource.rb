281bb58546229f015557554ee865b323c7d3cf61
Josh Justice <me@codingitwrong.com>
2018-12-30 09:00:01 -0600

    Compute last_read_at as N+1

--- app/resources/book_resource.rb
+++ app/resources/book_resource.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 class BookResource < ApplicationResource
-  attributes *%i[name num_chapters]
+  attributes *%i[name num_chapters last_read_at]
+
+  def last_read_at
+    current_user.readings
+                .where(book: @model)
+                .map(&:completed_at)
+                .compact
+                .max
+  end
 end
