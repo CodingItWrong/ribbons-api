b6c6f727ee2049aa6b45a7d1e0a4372e29a879ed
Josh Justice <josh.justice@accesscfa.com>
2020-06-26 17:37:29 -0400

    Go with default kebab-case

--- config/initializers/jsonapi_resources.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-JSONAPI.configure do |config|
-  # built in key format options are :underscored_key, :camelized_key and :dasherized_key
-  config.json_key_format = :camelized_key
-
-  #:underscored_route, :camelized_route, :dasherized_route, or custom
-  config.route_format = :camelized_route
-end
