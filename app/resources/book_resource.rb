cc274db18fba932b8aee8a6694a642a9a5147458
Josh Justice <me@codingitwrong.com>
2021-10-02 21:33:42 -0400

    Add book abbreviation

--- app/resources/book_resource.rb
+++ app/resources/book_resource.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class BookResource < ApplicationResource
-  attributes *%i[name singular_name num_chapters last_read_at]
+  attributes *%i[name singular_name abbreviation num_chapters last_read_at]
 
   def last_read_at
     current_user.readings.where(book: @model).map(&:completed_at).compact.max
