a178896f14ac33e01045a063d7329d9d75d52732
Josh Justice <me@codingitwrong.com>
2021-12-15 20:35:20 -0500

    Set legacy connection handling

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module BibleReading
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_record.legacy_connection_handling = false
   end
 end
